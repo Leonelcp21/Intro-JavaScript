2f8f5049741d3696408ac5dadae840bf67d750b5
Leonel Pereyra <leonelcpereyra@gmail.com>
2021-11-24 22:27:00 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,76 @@
+//variable
+//espacio de memoria donde guardo datos
+
+var nombre="carlos" //no
+let apellido= "sonsada" //si
+
+const pi=3.14
+
+//tipos de datos
+
+//string
+let cliente = "macanudo"
+
+//number 
+
+let numero1=25
+let numero2=62.45
+let numero3=-15
+
+//boolean
+let comprar=true
+let comer=false
+
+//null
+let billetera=null
+
+//undefined
+let caja
+
+//array
+let arreglo= ["pablo",654,true]
+
+//object
+
+let persona={
+    nombre: "jose",
+    apellido: "Perez",
+    edad: 45,
+    }
+
+    //operadores
+
+    //aritmeticos
+let suma= numero1+numero2
+let resta = numero1 - numero2
+let multiplicacion = numero1 * numero2
+let division = numero2 / numero1
+let resto = numero1 % numero2
+
+   //unitarios
+   ++numero1
+   --numero1
+
+   //relacionales
+console.log (numero1 == numero2)
+console.log (numero1 >= numero2)
+console.log (numero1 <= numero2)
+console.log (numero1 != numero2)
+
+console.log ("el resultado de la resta es " + resta)
+
+//negacion 
+let verdadero = true
+console.log (!verdadero)
+
+//tarea
+// creamos dos variables: numero1, numero2
+// mostramos por consola los resultados de las operaciones
+// sumamos dos numeros
+// restamos dos numeros
+// multiplicamos dos numeros
+// dividimos dos numeros
+// comparamos si el numero 1 es mayor o igual que numero 2
+// comparamos si el numero 1 es distinto a numero 2
+
+let valor = prompt ("tu nombre tetonerrrr")
