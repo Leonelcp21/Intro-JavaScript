54bc00b963ea5ea8e0ab47fcabd219ae44e52cec
Leonel Pereyra <leonelcpereyra@gmail.com>
2021-12-04 21:14:45 -0300

    Tareas

---
+++ Tarea/tarea.js
@@ -0,0 +1,9 @@
+let numero1=25
+let numero2=20
+
+console.log(numero1 + numero2)
+console.log(`la resta entre ${numero1} y ${numero2} es igual a ${numero1 - numero2}`)
+console.log(numero1 * numero2)
+console.log(numero1 / numero2)
+console.log(numero1 >= numero2)
+console.log(numero1 != numero2)
